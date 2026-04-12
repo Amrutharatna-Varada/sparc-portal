5242d4d9b7fdd99463a22693a063755823fdf4db
pujithasirisha <pujithamanivarada68@gmail.com>
2026-04-12 23:41:49 +0530

    dashboard + uploads improvements

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
 import Head from "next/head";
 
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
+    <main>
         <Head>
         <title>SP Portal</title>
         <link rel="icon" sizes="25x25" href="/logo-sp.jpeg" />
