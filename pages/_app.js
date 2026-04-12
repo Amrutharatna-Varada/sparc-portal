5dae666563f6f6fa0b6b3be861eac4f8b861f977
pujithasirisha <pujithamanivarada68@gmail.com>
2026-04-13 02:13:04 +0530

    dashboard + uploads improvements

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <main>
         <Head>
+            <meta name="google-site-verification" content="google87adc8f0f582e95a.html" />
         <title>SP Portal</title>
         <link rel="icon" sizes="25x25" href="/logo-sp.jpeg" />
       </Head>
