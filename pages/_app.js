a837ec384a8e86bf87e31038abd4b82254596f51
pujithasirisha <pujithamanivarada68@gmail.com>
2026-05-09 23:17:34 +0530

    reverted changes

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,6 @@ export default function App({ Component, pageProps }) {
             <meta name="google-site-verification" content="yLlKvA-YZ6bIEcgI6CA4f1qCPrZ6SGWzRl5TEfz17fk" />
         <title>SP Portal</title>
         <link rel="icon" sizes="25x25" href="/logo-sp.jpeg" />
-
-        <meta
-    name="description"
-    content="SP Architects & Constructions - villa design, floor plans, construction services."
-  />
-
-  <meta
-    name="keywords"
-    content="architecture, villa design, construction, SP Architects"
-  />
       </Head>
       <Component {...pageProps} />
     </main>
